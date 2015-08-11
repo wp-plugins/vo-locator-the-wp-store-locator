afdad572598447bc512ccec0dd7c2982b3b5e878
Jurski <Jurski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 18:08:08 +0000

    File Updated
    git-svn-id: https://plugins.svn.wordpress.org/vo-locator-the-wp-store-locator/trunk@1218432 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vo-locator.php
+++ vo-locator.php
@@ -3,18 +3,17 @@
 Plugin Name: VO Locator
 Plugin URI: http://www.vitalorganizer.com/vo-locator-wordpress-store-locator-plugin/
 Description: Simple wordpress store locator plugin to manage multiple business locations and other any places using Google Maps. Manage a few or thousands of locations effortlessly with setup in minutes.
-Version: 1.0
+Version: 1.0.1
 Author: Jurski
 Author URI: http://www.vitalorganizer.com
 */
-$vosl_version="1.0";
+$vosl_version="1.0.1";
 define('VOSL_VERSION', $vosl_version);
 $vosl_db_version=1.0;
 include_once("vosl-define.php");
+include_once("vosl-functions.php");
 
 add_action('admin_menu', 'vosl_add_options_page');
-
-include_once("vosl-functions.php");
 
 register_activation_hook( __FILE__, 'vosl_install_tables');
 
